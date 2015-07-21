95318ab4a9202b891237bf59e40744c4c2a12bb7
Daniel Williams <dwilliams@inovonics.com>
2015-07-21 10:05:41 -0600

    Trying to integrate celery.

--- hasts/__init__.py
+++ hasts/__init__.py
@@ -3,7 +3,7 @@
 ### IMPORTS ###
 import uuid
 
-from celery import Celery
+from celerytasks import makeCelery
 
 from flask import Flask
 from flask import abort, render_template, url_for
@@ -21,18 +21,6 @@ app.config.update( CELERY_BROKER_URL = rmqBrokerURL )
 celery = makeCelery( app)
 
 ### FUNCTIONS ###
-def makeCelery( app):
-    celery = Celery( app.import_name, broker = app.config[ 'CELERY_BROKER_URL'])
-    celery.conf.update( app.config)
-    TaskBase = celery.Task
-    class ContextTask( TaskBase):
-        abstract = True
-        def __call__( self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__( self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
 @app.route( '/')
 def helloWorld():
     """Display the index page.  This will eventually load a webapp that will access the APIs."""
@@ -75,4 +63,3 @@ def showRoom( room_uuid):
 ### MAIN ###
 if __name__ == '__main__':
     pass
-
---
+++ hasts/celerytasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+### IMPORTS ###
+import uuid
+
+from celery import Celery
+
+### GLOBALS ###
+
+### FUNCTIONS ###
+def makeCelery( app):
+    celery = Celery( app.import_name, broker = app.config[ 'CELERY_BROKER_URL'])
+    celery.conf.update( app.config)
+    TaskBase = celery.Task
+    class ContextTask( TaskBase):
+        abstract = True
+        def __call__( self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__( self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+### CLASSES ###
+
+### MAIN ###
+if __name__ == '__main__':
+    pass
