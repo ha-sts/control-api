08b144ba3f5e4a9a3bda9d311511230a15f04b28
Daniel Williams <dwilliams@inovonics.com>
2015-07-21 10:01:30 -0600

    Trying to integrate the flask-celery example.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A flask based API for controlling the system.
 ## Requirements
 * Docker
 * RabbitMQ Docker image
-  * `docker run -d --hostname <hostname> --name <imagename> -p 15672:15672 rabbitmq:3.5-management`
+  * `docker run -d --hostname <hostname> --name <imagename> -p 5672:5672 -p 15672:15672 rabbitmq:3.5-management`
--- hasts/__init__.py
+++ hasts/__init__.py
@@ -3,13 +3,36 @@
 ### IMPORTS ###
 import uuid
 
+from celery import Celery
+
 from flask import Flask
 from flask import abort, render_template, url_for
 
 ### GLOBALS ###
 app = Flask( __name__)
 
+# This needs to be handled in a better way
+rmqHost = 'localhost'
+rmqUser = 'guest'
+rmqPass = 'guest'
+rmqPort = '5672'
+rmqBrokerURL = 'amqp://%s:%s@%s:%s//' % ( rmqUser, rmqPass, rmqHost, rmqPort)
+app.config.update( CELERY_BROKER_URL = rmqBrokerURL )
+celery = makeCelery( app)
+
 ### FUNCTIONS ###
+def makeCelery( app):
+    celery = Celery( app.import_name, broker = app.config[ 'CELERY_BROKER_URL'])
+    celery.conf.update( app.config)
+    TaskBase = celery.Task
+    class ContextTask( TaskBase):
+        abstract = True
+        def __call__( self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__( self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
 @app.route( '/')
 def helloWorld():
     """Display the index page.  This will eventually load a webapp that will access the APIs."""
