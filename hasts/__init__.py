2391daf6654ff59d8b3b86f5a595dbd85f6d5632
Daniel Williams <dwilliams@inovonics.com>
2015-07-21 09:33:00 -0600

    Adding in some place holders for API functions.

--- hasts/__init__.py
+++ hasts/__init__.py
@@ -8,8 +8,29 @@ app = Flask( __name__)
 
 ### FUNCTIONS ###
 @app.route( '/')
-def hello_world():
+def helloWorld():
+    """Display the index page.  This will eventually load a webapp that will access the APIs."""
     return 'Hello World!'
+
+@app.route( '/devices/')
+def showDevices();
+    """Display a list of devices."""
+    return 'Device list'
+
+@app.route( '/devices/<device_id>')
+def showDevice( device_id):
+    """Display information about a device in the system."""
+    return 'Device %d.' % ( device_id, )
+
+@app.route( '/rooms/')
+def showRooms();
+    """Display a list of rooms."""
+    return 'Room list'
+
+@app.route( '/rooms/<room_id>')
+def showRoom( room_id):
+    """Display information about a room in the system."""
+    return 'Room %d.' % ( room_id, )
 
 ### CLASSES ###
 
